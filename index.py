8353509e4bcda1632c43d042adcba410e4c346ef
benno <noyesbenjamin@gmail.com>
2021-08-25 15:06:06 -0400

    mobile

--- index.py
+++ index.py
@@ -103,6 +103,6 @@ def display_page(pathname):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, port=8000, 
-        # host='0.0.0.0'
+    app.run_server(debug=False, 
+        host='0.0.0.0'
         )
