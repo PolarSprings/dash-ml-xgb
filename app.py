97f4c6ac68f3593c7fb04ac479b8014e7ac4b7ef
benno <noyesbenjamin@gmail.com>
2021-08-27 21:59:44 -0400

    static

--- app.py
+++ app.py
@@ -2,10 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 from whitenoise import WhiteNoise
 
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.MINTY],
-                meta_tags=[{'name': 'viewport',
-                            'content': 'width=device-width, initial-scale=1.0'}]
-                )
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.MINTY])
 app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_locally=True
 server = app.server
