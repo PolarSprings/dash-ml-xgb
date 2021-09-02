e2d69361b390b30b5e863f51f1bb4db98db0c0fc
benno <noyesbenjamin@gmail.com>
2021-09-02 13:09:14 -0400

    app update with newrelic: import agent

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
 from whitenoise import WhiteNoise
-
+from newrelic import agent
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.MINTY])
+app = agent.WSGIApplicationWrapper(app)
 app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_locally=True
 server = app.server
