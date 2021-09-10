24a2f888b4740dfb440f713122278dc4e80a64e1
benno <noyesbenjamin@gmail.com>
2021-09-10 16:23:17 -0400

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_bootstrap_components as dbc
 from whitenoise import WhiteNoise
 from newrelic import agent
+
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.MINTY])
 app = agent.WSGIApplicationWrapper(app)
 app.config.suppress_callback_exceptions = True
